2b6575846475c900cda98a7b63c31216a4b9ea8c
zuiguangyin <2247359268@qq.com>
2021-01-11 01:24:30 +0800

    🥍🥍🥍如何在newWindow之中开启右键菜单呢

--- electron-test/render/index.js
+++ electron-test/render/index.js
@@ -20,18 +20,19 @@ window.onload = function () {
         })
         newWin.loadFile('views/newWindow.html')
 
-        const { remote } = require('electron')
-        var rigthTemplate = [
-            { label: '粘贴' ,accelerator:'ctrl+c'},
-            { label: '复制' ,accelerator:'ctrl+v'}
-        ]
-        var m = remote.Menu.buildFromTemplate(rigthTemplate)
-        window.addEventListener('contextmenu', function (e) {
-            //阻止当前窗口默认事件
-            e.preventDefault();
-            //把菜单模板添加到右键菜单
-            m.popup({ window: remote.getCurrentWindow() })
-        })
+        // 如何在newWindow之中开启右键菜单呢
+        // const { remote } = require('electron')
+        // var rigthTemplate = [
+        //     { label: '粘贴' ,accelerator:'ctrl+c'},
+        //     { label: '复制' ,accelerator:'ctrl+v'}
+        // ]
+        // var m = remote.Menu.buildFromTemplate(rigthTemplate)
+        // window.addEventListener('contextmenu', function (e) {
+        //     //阻止当前窗口默认事件
+        //     e.preventDefault();
+        //     //把菜单模板添加到右键菜单
+        //     m.popup({ window: remote.getCurrentWindow() })
+        // })
 
         // 监听关闭事件, 关闭之后销毁窗口, 防止占用太多内存
         newWin.on('closed', () => { newWin = null })
