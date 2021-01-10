0359a7e21440334002e8410e3d1caa44a215fb04
zuiguangyin <2247359268@qq.com>
2021-01-10 23:42:42 +0800

    🎃🎃🎃使用remote创建了新的窗口

--- electron-test/render/index.js
+++ electron-test/render/index.js
@@ -4,10 +4,23 @@ var fs = require('fs')
 window.onload = function () {
     var btn = this.document.querySelector('#btn')
     var mybaby = this.document.querySelector('#mybaby')
-    btn.onclick = function(){
-        fs.readFile('xiaojiejie.txt',(err,data)=>{
+    btn.onclick = function () {
+        fs.readFile('xiaojiejie.txt', (err, data) => {
             mybaby.innerHTML = data
         })
     }
+
+    // 在子进程之中使用remote的功能，下面是要创建一个新的页面
+    let createBtn = this.document.querySelector('#createWindow')
+    let BrowserWindow = require('electron').remote.BrowserWindow
+    createBtn.onclick = () => {
+        let newWin = new BrowserWindow({
+            width: 500,
+            height: 500,
+        })
+        newWin.loadFile('newWindow.html')
+        newWin.on('close', () => { newWin = null })
+    }
+
 }
 
